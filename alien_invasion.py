5d5a015614ed9a303f618b9449dce807d50adf12
Necromantiq <daniel.kross.79@protonmail.com>
2021-07-28 12:42:09 +0300

    ship now moving, discrete moving

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,7 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -22,6 +21,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     self.running = False
                     #sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.rect.x += 10
+                    if event.key == pygame.K_LEFT:
+                        self.ship.rect.x -= 10
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
