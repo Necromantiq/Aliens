9b59f46cfa26517772b1171c5c3c4dd305c686a3
Necromantiq <daniel.kross.79@protonmail.com>
2021-07-28 17:44:54 +0300

    created handle for ship, changed settings.py, ship.py, alien_ivasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
+
 import pygame
+
 from settings import Settings
 from ship import Ship
 
@@ -9,48 +11,56 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self._running = True
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        self.ship = Ship(self)
-        # Флаг перемещения
-        self.moving_right = False
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
-
-    
-    def _check_events(self):
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    #sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.rect.x += 10
-                    if event.key == pygame.K_LEFT:
-                        self.ship.rect.x -= 10
-
-    def update(self):
-        """Обновляет позицию корабля с учетом флага."""
-        if self.moving_right:
-            self.rect.x += 1
-
-
-    def _update_screen(self):
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+       
+        self.ship = Ship(self)
 
 
     def run_game(self):
         """Start the main loop for the game."""
-        self.running = True
-        while self.running:
-            # Watch for keyboard and mouse events.
+        while self._running:
             self._check_events()
-
-            # Redraw the screen during each pass through the loop.
+            self.ship.update()
             self._update_screen()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._running = False
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
