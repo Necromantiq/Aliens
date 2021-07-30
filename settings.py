dd70711bb4d4d5fb801c0dd646a9036c445ed449
Necromantiq <daniel.kross.79@protonmail.com>
2021-07-31 00:45:54 +0300

    added settings for bullets in settings.py

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 0.5
+
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
